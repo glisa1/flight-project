944bf252ae4ad5235e29671461d328f60db30966
Danijel <glisovic.danijel96@gmail.com>
2024-11-12 19:22:02 +0100

    Added identity.

--- FlightProject/FlightProject.Domain/Database/AppDbContext.cs
+++ FlightProject/FlightProject.Domain/Database/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using FlightProject.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlightProject.Domain.Database;
 
-internal class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+internal class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Flight> Flights { get; set; }
     public DbSet<City> Cities { get; set; }
