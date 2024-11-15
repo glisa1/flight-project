fad61a349db3c140128cdf90f18b9f9dc908a6b7
Danijel <glisovic.danijel96@gmail.com>
2024-11-15 12:36:14 +0100

    Blazor init change.

---
+++ FlightProject/FlightProject.UIClient/Models/FlightDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightProject.UIClient.Models;
+
+public record FlightDto
+    (
+        int FlightId,
+        double Price,
+        DateTime Departure,
+        DateTime Arrival,
+        string DepartureCityName,
+        string DestinationCityName,
+        int NumberOfSeats
+    );
