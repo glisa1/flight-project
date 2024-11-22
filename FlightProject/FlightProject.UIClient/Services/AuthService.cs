d650512c778bb5961098710683ec2e8effa6c069
Danijel <glisovic.danijel96@gmail.com>
2024-11-22 20:06:57 +0100

    Added empty auth service.

---
+++ FlightProject/FlightProject.UIClient/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FlightProject.UIClient.Services;
+
+public class AuthService(string endpoint) : IAuthService
+{
+    private readonly string _apiEndpoint = endpoint;
+    private readonly HttpClient _httpClient = new();
+
+    public Task<bool> AuthenticateAsync(string username, string password)
+    {
+        _httpClient.
+    }
+
+    public Task Logut()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FlightProject/FlightProject.UIClient/Services/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace FlightProject.UIClient.Services;
+
+public interface IAuthService
+{
+    Task<bool> AuthenticateAsync(string username, string password);
+    Task Logut();
+}
